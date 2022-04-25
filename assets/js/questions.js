d914f5cb2c5816faa45c9f794e1e6934bef68909
katredford <katharine.redford@gmail.com>
2022-04-25 16:25:55 -0500

    first commit

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+	{
+		title:
+			"how long does it take for a photon in the center of the sun to reach the surface?",
+		correct: "one million years",
+		answers: [
+			"one million years",
+			"6 years",
+			"one billion years",
+			"one trillion years",
+		],
+	},
+
+	{
+		title: "which isnt a primary color?",
+		correct: "green",
+		answers: ["red", "green", "blue", "yellow"],
+	},
+
+	{
+		title: "which criptid is the best?",
+		correct: "nessie",
+		answers: ["mothman", "bigfoot", "hodag", "nessie"],
+	},
+
+	{
+		title: "what to monarch caterpillars eat?",
+		correct: "milkweed",
+		answers: ["sandwich", "parsley", "milkweed", "daffodils"],
+	},
+
+	{
+		title: "who stole fire from the gods?",
+		correct: "prometheus",
+		answers: ["medusa", "prometheus", "hercules", "perseus"],
+	},
+];
